c4f9a15ca7b0e913b2bc91f6316d6da9b4221648
Andrew Vojak <andrew.vojak@gmail.com>
2021-03-31 01:46:34 +0100

    Update requirements

--- setup.py
+++ setup.py
@@ -10,7 +10,12 @@ setup(
         description="View Raspberry Pi stats on a tiny OLED display",
         license="Apache License, Version 2.0",
         packages=find_packages(),
-        install_requires=[],
+        install_requires=[
+            'adafruit-circuitpython-ssd1306>=2.11.2',
+            'gpiozero>=1.6.2',
+            'Pillow>=5.4.1'
+            'psutil>=5.8.0'
+        ],
         keywords=['python', 'pi', 'raspberry', 'oled', 'stats'],
         classifiers=[
         ]
