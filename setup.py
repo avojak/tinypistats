fb26c482e8a3c35352fdc249d72764f3c92bb017
Andrew Vojak <andrew.vojak@gmail.com>
2021-04-29 03:39:56 +0100

    Add more project files

--- setup.py
+++ setup.py
@@ -1,6 +1,11 @@
-from setuptools import setup
+from setuptools import setup, find_packages
+import pathlib
 
-VERSION='0.0.1'
+here = pathlib.Path(__file__).parent.resolve()
+
+long_description = (here / 'README.md').read_text(encoding='utf-8')
+
+VERSION='1.0.0'
 
 setup(
         name="tinypistats",
@@ -8,6 +13,7 @@ setup(
         author="Andrew Vojak",
         author_email="andrew.vojak@gmail.com",
         description="View Raspberry Pi stats on a tiny OLED display",
+        long_description=long_description,
         license="Apache License, Version 2.0",
         packages=find_packages(),
         install_requires=[
@@ -18,5 +24,16 @@ setup(
         ],
         keywords=['python', 'pi', 'raspberry', 'oled', 'stats'],
         classifiers=[
-        ]
+            'Programming Language :: Python :: 3'
+            'Programming Language :: Python :: 3.6'
+            'Programming Language :: Python :: 3.7'
+            'Programming Language :: Python :: 3.8'
+            'Programming Language :: Python :: 3.9'
+            'Programming Language :: Python :: 3 :: Only'
+        ],
+        python_requires='>=3.6, <4',
+        project_urls={
+            'Bug Reports': 'https://github.com/avojak/tinypistats/issues',
+            'Source': 'https://github.com/avojak/tinypistats'
+        }
 )
